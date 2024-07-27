ea7c5c701b45abd8ce642238d348eb2460482613
Pranav Bawgikar <102728016+PranavBawgikar@users.noreply.github.com>
2024-07-27 13:35:42 +0530

    Added chevron icon and polished the code.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,80 @@
-import { FolderIcon } from "@heroicons/react/24/solid"
+'use client'
+import { ChevronRightIcon, DocumentIcon, FolderIcon } from "@heroicons/react/24/solid"
+import { useState } from "react"
 
-type Folder = {
+type Node = {
     name: string
-    folders?: Folder[]
+    nodes?: Node[]
 }
 
-let folders: Folder[] = [
+let nodes: Node[] = [
 {
-    name: 'Home', folders: [
-        { name: 'Movies', folders: [
-            { name: 'Sci-fi', folders: [
-                { name: '2021' },
-                { name: '2017' }
+    name: 'Home', nodes: [
+        { name: 'Movies', nodes: [
+            { name: 'Sci-fi', nodes: [
+                { name: '2021', nodes: [
+                    { name: 'dune-part-one.mp4'},
+                    { name: 'spider-man-no-way-home.mp4'}
+                ] },
+                { name: '2017', nodes: [
+                    { name: 'blade-runner-2049.mp4'}
+                ] }
             ]},
-            { name: 'Adventure' }
+            { name: 'Adventure', nodes: [] }
         ]},
-        { name: 'Anime', folders: [
-            { name: '2021' },
-            { name: '2019' }
+        { name: 'Anime', nodes: [
+            { name: '2021', nodes: [
+                { name: 'tokyo-revengers.mp4' }
+            ] },
+            { name: '2019', nodes: [
+                {name: 'demon-slayer.mp4'}
+            ] }
         ] }, 
-        { name: 'Pictures' }, 
-        { name: 'Documents' },
-        { name: 'Anime' }
+        { name: 'Pictures', nodes: [] }, 
+        { name: 'Documents',nodes: [] },
+        { name: 'passwords.txt' }
     ]
 }
 ]
 
 export default function Index() {
     return (
-        <div className="p-8 max-w-sm mx-auto">
+        <div className="p-8 max-w-lg mx-auto">
             <ul>
-                { folders.map((folder) => (
-                    <Folder folder={ folder } key={ folder.name } />
+                { nodes.map((node) => (
+                    <FileSystemItem node={ node } key={ node.name } />
                 ))}
             </ul>
         </div>
     )
 }
 
-function Folder({ folder }: { folder: Folder }) {
+function FileSystemItem({ node }: { node: Node }) {
+
+    let [ isOpen, setIsOpen ] = useState(false)
+
     return (
-        <li className="my-1.5" key={folder.name}>
+        <li className="my-1.5" key={node.name}>
             <span className="flex items-center gap-1.5">
-                <FolderIcon className="size-6 text-sky-500" />
-                {folder.name}
+                { node.nodes && node.nodes.length > 0 && (
+                    <button onClick={() => setIsOpen(!isOpen)}>
+                        <ChevronRightIcon className={`size-4 text-gray-500 ${ isOpen ? 'rotate-90' : '' }`} />   
+                    </button>
+                )}
+                { node.nodes? (
+                    <FolderIcon className={`size-6 text-sky-500 ${node.nodes.length === 0 ? 'ml-[22px]' : ''}`} />
+                ) : (
+                    <DocumentIcon className="ml-[22px] size-6 text-gray-900" />
+                )}
+                {node.name}
             </span>
-            <ul className="pl-6">
-                { folder.folders?.map(folder => (
-                    <Folder folder={ folder } key={ folder.name }/> // Recursive component
-                ))}
-            </ul>
+            {isOpen && (
+                <ul className="pl-6">
+                    { node.nodes?.map(folder => (
+                        <FileSystemItem node={ folder } key={ folder.name }/> // Recursive component
+                    ))}
+                </ul>
+            )}
         </li>
     )
 }
